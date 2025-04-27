add805adfe506924c6db6c525deba52d5e502e9c
thinkramen <thinkramen@gmail.com>
2025-04-26 23:03:05 -0700

    made functions async to work properly.

--- background.js
+++ background.js
@@ -1,95 +1,136 @@
 chrome.runtime.onInstalled.addListener(() => {
-  // set default values for storage
-  chrome.storage.local.set({ isEnabled: true }, () => {
-    console.log("Extension installed and enabled by default.");
-  });
+	// set default values for storage
+	chrome.storage.local.set({ isEnabled: true }, () => {
+		console.log("Extension installed and enabled by default.");
+	});
 });
 
 chrome.action.onClicked.addListener(() => {
-  chrome.runtime.openOptionsPage();
+	chrome.runtime.openOptionsPage();
 });
 
 // on startup, check for inventory updates
 chrome.runtime.onStartup.addListener(() => {
-  checkForInventoryUpdates();
+	checkForInventoryUpdates();
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "checkForInventoryUpdates") {
-    console.log("checking for inventory updates...");
-    try {
-      checkForInventoryUpdates();
-      console.log("inventory check completed.");
-      sendResponse({ success: true });
-    } catch (error) {
-      console.error("Error checking for inventory updates:", error);
-      sendResponse({ success: false, error: error.message });
-    }
-  }
+	if (request.action === "checkForInventoryUpdates") {
+		console.log("checking for inventory updates...");
+		try {
+			checkForInventoryUpdates();
+			console.log("inventory check completed.");
+			sendResponse({ success: true });
+		} catch (error) {
+			console.error("Error checking for inventory updates:", error);
+			sendResponse({ success: false, error: error.message });
+		}
+	}
 });
 
 // configure the time interval for checking inventory updates
 let CHECK_INTERVAL = 10; // 10 minutes
-let CHECK_INTERVAL_TEST = 1; // 1 minute for testing
-CHECK_INTERVAL = CHECK_INTERVAL_TEST; // set to test interval for testing
 // alarm setup to poll every 10 minutes
 chrome.alarms.create("inventoryCheck", {
-  periodInMinutes: CHECK_INTERVAL,
+	periodInMinutes: CHECK_INTERVAL,
 });
 // alarm listener
 chrome.alarms.onAlarm.addListener((alarm) => {
-  if (alarm.name === "inventoryCheck") {
-    // Fetch the inventory from the target URL
-    checkForInventoryUpdates();
-    console.log("checking for inventory updates...");
-  }
-});
-
-// when local storage is updated, check for updates
-chrome.storage.onChanged.addListener((changes, area) => {
-  console.log("storage modifed...\n");
-  if (area === "local" && changes.searches) {
-    console.log("searches updated...\n", changes.searches.newValue);
-    // check for inventory updates
-    checkForInventoryUpdates();
-  }
+	if (alarm.name === "inventoryCheck") {
+		// Fetch the inventory from the target URL
+		checkForInventoryUpdates();
+		console.log("checking for inventory updates...");
+	}
 });
 
 // check for inventory updates
 async function checkForInventoryUpdates() {
-  // get searches from chrome storage
-  let searches = (await chrome.storage.local.get("searches")) || [];
-  console.log("getting searches from local storage...\n", searches);
-  // for every search in searches, fetch inventory from their url
-  searches = searches.searches || []; // ensure searches is an array
-  console.log("searches:\n", searches);
-  // TODO: build a api call from the url and fetch the data
-  // https://www.picknpull.com/api/vehicle/search?&makeId=90&modelId=1150&year=&distance=50&zip=95843&language=english
-  // required params: makeId, modelId, distance, zip - max distance 25000 mi = unlimited
-  // optional params: year, language
+	// get searches from chrome storage
+	let searches = (await chrome.storage.local.get("searches")) || [];
+	console.log("getting searches from local storage...\n", searches);
+	// for every search in searches, fetch inventory from their url
+	searches = searches.searches || []; // ensure searches is an array
+	console.log("searches:\n", searches);
+	const apiUrl = new URL("https://www.picknpull.com/api/vehicle/search");
+	// required params: makeId, modelId, distance, zip - max distance 25000 mi = unlimited
+	// optional params: year, language
+	searches.forEach(async (search) => {
+		// build the api call from the url
+		function buildAPICall(params) {
+			const sp = new URLSearchParams();
+			// Required parameters
+			if (params.make) sp.append("makeId", params.make);
+			if (params.model) sp.append("modelId", params.model);
 
-  // searches.forEach(async (search) => {
-  //   try {
-  //     const response = await fetch(search.url, {
-  //       credentials: "include",
-  //       headers: {
-  //         Origin: "https://www.picknpull.com",
-  //       },
-  //     });
-  //     const data = await response.json();
-  //     console.log("data:\n", data);
-  //   } catch (error) {
-  //     console.error(`Failed to fetch ${search.url}:`, error);
-  //   }
-  // });
+			// Optional parameters
+			if (params.year !== undefined) sp.append("year", params.year || "");
+			sp.append("language", "english"); // default to english
+			if (params.distance) sp.append("distance", params.distance);
+			if (params.zip) sp.append("zip", params.zip);
+			return `${apiUrl}?&${sp.toString()}`;
+		}
+		const apiCall = buildAPICall(search.params);
+		// call the api and get the data
+		try {
+			const response = await fetch(apiCall, {
+				credentials: "include",
+				headers: {
+					Origin: "https://www.picknpull.com",
+				},
+			});
+			const data = await response.json();
+			console.log("data:\n", data);
+			// extract the vehicle inventory from the data only.
+			const inventory = data.flatMap((location) => {
+				return location.vehicles.map((vehicle) => {
+					return {
+						id: vehicle.id,
+						make: vehicle.make,
+						model: vehicle.model,
+						year: vehicle.year,
+						vin: vehicle.vin,
+						row: vehicle.row,
+						location: vehicle.locationName,
+						dateAdded: vehicle.dateAdded,
+						image: vehicle.largeImage,
+					};
+				});
+			});
+			console.log("inventory:\n", inventory);
+			// check if the inventory is empty
+			if (inventory.length === 0) {
+				console.log("no inventory found for this search.");
+				return;
+			}
+			// set the data in chrome storage
+			// initialize the inventory array if it doesn't exist
+			if (!search.inventory) {
+				search.inventory = [];
+			}
+			search.inventory = inventory;
+			// sort the inventory by date added
+			search.inventory = [...search.inventory].sort(
+				(a, b) => new Date(b.dateAdded) - new Date(a.dateAdded)
+			);
+			if (search.inventory.lengh < inventory.length) {
+				console.log("new inventory found for this search.");
+			}
+
+			chrome.storage.local.set({ searches }, () => {
+				console.log("inventory updated in local storage.");
+			});
+		} catch (error) {
+			console.error(`Failed to fetch ${search.url}:`, error);
+		}
+	});
 }
 
 // notification function
 function notifyUser(title, message) {
-  chrome.notifications.create({
-    type: "basic",
-    iconUrl: "icon.png",
-    title: title,
-    message: message,
-  });
+	chrome.notifications.create({
+		type: "basic",
+		iconUrl: "icon.png",
+		title: title,
+		message: message,
+	});
 }
--- settings.js
+++ settings.js
@@ -1,60 +1,59 @@
-// initialize searches
-let searches = [];
-chrome.storage.local.get("searches", (result) => {
-  searches = result.searches || [];
-  updateSearches();
+document.addEventListener("DOMContentLoaded", async () => {
+	updateSearches();
 });
-
 // event listener for save button
 document.getElementById("save-search").addEventListener("click", () => {
-  saveSearch();
-  updateSearches();
+	saveSearch();
+	updateSearches();
 });
 
 // save search logic
-function saveSearch() {
-  const urlInput = document.getElementById("search-url").value.trim();
-  const nameInput = document.getElementById("search-name").value.trim();
+async function saveSearch() {
+	const searches = (await chrome.storage.local.get("searches")).searches || [];
+	console.log("getting searches from local storage...\n", searches);
+	const urlInput = document.getElementById("search-url").value.trim();
+	const nameInput = document.getElementById("search-name").value.trim();
 
-  if (!urlInput || !nameInput) {
-    showToast("please enter both a name and a url before saving.");
-    return;
-  }
+	if (!urlInput || !nameInput) {
+		showToast("please enter both a name and a url before saving.");
+		return;
+	}
 
-  if (!isValidUrl(urlInput) || !urlInput.includes("picknpull.com")) {
-    showToast("please enter a valid pick-n-pull url ⚠");
-    return;
-  }
+	if (!isValidUrl(urlInput) || !urlInput.includes("picknpull.com")) {
+		showToast("please enter a valid pick-n-pull url ⚠");
+		return;
+	}
 
-  if (searches.some((s) => s.name === nameInput)) {
-    showToast("this name already exists ⚠");
-    return;
-  }
+	if (searches.some((s) => s.name === nameInput)) {
+		showToast("this name already exists ⚠");
+		return;
+	}
 
-  if (searches.some((s) => s.url === urlInput)) {
-    showToast("this url already exists ⚠");
-    return;
-  }
-  const params = Object.fromEntries(new URL(urlInput).searchParams.entries());
-  console.log("params:\n", params);
-  searches.push({ name: nameInput, url: urlInput, params: params });
-  chrome.storage.local.set({ searches }, () => {
-    showToast("search saved successfully 💾");
-  });
+	if (searches.some((s) => s.url === urlInput)) {
+		showToast("this url already exists ⚠");
+		return;
+	}
+	const params = Object.fromEntries(new URL(urlInput).searchParams.entries());
+	console.log("params:\n", params);
+	searches.push({ name: nameInput, url: urlInput, params: params });
+	await chrome.storage.local.set({ searches });
+	showToast("search saved successfully 💾");
+	updateSearches();
 }
 
 // update ui with saved searches
-function updateSearches() {
-  const savedSearches = document.getElementById("saved-searches");
-  savedSearches.innerHTML = ""; // clear existing searches
+async function updateSearches() {
+	const searches = (await chrome.storage.local.get("searches")).searches || [];
+	const savedSearches = document.getElementById("saved-searches");
+	savedSearches.innerHTML = ""; // clear existing searches
 
-  searches.forEach((search) => {
-    const searchItem = document.createElement("div");
-    const params = Object.entries(search.params)
-      .map(([key, value]) => `${key}: ${value}`)
-      .join(" ");
-    searchItem.className = "search-item";
-    searchItem.innerHTML = `
+	searches.forEach((search) => {
+		const searchItem = document.createElement("div");
+		const params = Object.entries(search.params)
+			.map(([key, value]) => `${key}: ${value}`)
+			.join(" ");
+		searchItem.className = "search-item";
+		searchItem.innerHTML = `
                         <h3>${search.name}</h3>
                         <h3>${params}</h3>
                         <h3>${search.url}</h3>
@@ -62,82 +61,83 @@ function updateSearches() {
                         <button class="delete-search">delete 🚮</button>
                 `;
 
-    // add event listener to delete button
-    searchItem.querySelector(".delete-search").addEventListener("click", () => {
-      deleteSearch(search.name);
-    });
-    // add event listener to launch button
-    searchItem.querySelector(".launch-search").addEventListener("click", () => {
-      launchSearch(search.url);
-    });
+		// add event listener to delete button
+		searchItem.querySelector(".delete-search").addEventListener("click", () => {
+			deleteSearch(search.name);
+		});
+		// add event listener to launch button
+		searchItem.querySelector(".launch-search").addEventListener("click", () => {
+			launchSearch(search.url);
+		});
 
-    savedSearches.appendChild(searchItem);
-  });
+		savedSearches.appendChild(searchItem);
+	});
 }
 
 // delete a search
-function deleteSearch(name) {
-  searches = searches.filter((item) => item.name !== name);
-  chrome.storage.local.set({ searches }, () => {
-    updateSearches();
-    showToast("search deleted successfully 🚮");
-  });
+async function deleteSearch(name) {
+	let searches = (await chrome.storage.local.get("searches")).searches || [];
+	searches = searches.filter((item) => item.name !== name);
+	await chrome.storage.local.set({ searches });
+	updateSearches();
+	showToast("search deleted successfully 🚮");
 }
 
 // launch search
 function launchSearch(url) {
-  chrome.tabs.create({ url });
-  showToast("launching search.. 🚀");
+	chrome.tabs.create({ url });
+	showToast("launching search.. 🚀");
 }
 
 // function to check for updates
 async function checkForUpdates() {
-  chrome.runtime.sendMessage(
-    { action: "checkForInventoryUpdates" },
-    (response) => {
-      if (response && response.success) {
-        console.log("inventory check completed.");
-        updateSearches();
-      } else {
-        console.error("Error checking for inventory updates:", response);
-      }
-    }
-  );
+	const response = await chrome.runtime.sendMessage(
+		{ action: "checkForInventoryUpdates" },
+		(response) => {
+			if (response && response.success) {
+				console.log("inventory check completed.");
+				updateSearches();
+			} else {
+				console.error("Error checking for inventory updates:", response);
+			}
+		}
+	);
+	return response;
 }
 const checkUpdatesButton = document.getElementById("check-for-updates");
 checkUpdatesButton.addEventListener("click", () => {
-  checkForUpdates();
-  showToast("checking for updates... 🔄");
+	checkForUpdates();
+	showToast("checking for updates... 🔄");
 });
 
 // validate url
 function isValidUrl(url) {
-  const urlRegex = new RegExp(
-    "^(https?:\\/\\/)?" + // protocol (optional)
-      "((([a-zA-Z0-9\\-]+\\.)+[a-zA-Z]{2,})|" + // domain name
-      "localhost|" + // localhost
-      "\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}|" + // ip address
-      "\\[?[a-fA-F0-9]*:[a-fA-F0-9:]+\\]?)" + // ipv6
-      "(\\:\\d+)?(\\/[-a-zA-Z0-9@:%_+.~#?&//=]*)?$"
-  );
-  return urlRegex.test(url);
+	const urlRegex = new RegExp(
+		"^(https?:\\/\\/)?" + // protocol (optional)
+			"((([a-zA-Z0-9\\-]+\\.)+[a-zA-Z]{2,})|" + // domain name
+			"localhost|" + // localhost
+			"\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}|" + // ip address
+			"\\[?[a-fA-F0-9]*:[a-fA-F0-9:]+\\]?)" + // ipv6
+			"(\\:\\d+)?(\\/[-a-zA-Z0-9@:%_+.~#?&//=]*)?$"
+	);
+	return urlRegex.test(url);
 }
 
 // show toast notification
 function showToast(message) {
-  const toast = document.createElement("div");
-  toast.textContent = message;
-  Object.assign(toast.style, {
-    position: "fixed",
-    bottom: "20px",
-    right: "20px",
-    backgroundColor: "#333",
-    color: "#fff",
-    padding: "10px 20px",
-    borderRadius: "5px",
-    boxShadow: "0 2px 5px rgba(0, 0, 0, 0.3)",
-    zIndex: "1000",
-  });
-  document.body.appendChild(toast);
-  setTimeout(() => toast.remove(), 3000);
+	const toast = document.createElement("div");
+	toast.textContent = message;
+	Object.assign(toast.style, {
+		position: "fixed",
+		bottom: "20px",
+		right: "20px",
+		backgroundColor: "#333",
+		color: "#fff",
+		padding: "10px 20px",
+		borderRadius: "5px",
+		boxShadow: "0 2px 5px rgba(0, 0, 0, 0.3)",
+		zIndex: "1000",
+	});
+	document.body.appendChild(toast);
+	setTimeout(() => toast.remove(), 3000);
 }
